7b333174db6c71576514ae41e2dcc6144dc855b5
barros9 <giu.barreca@gmail.com>
2024-08-18 15:07:25 +0200

    Added database

--- src/main/kotlin/com/barros9/tastytalesbackend/plugins/Databases.kt
+++ src/main/kotlin/com/barros9/tastytalesbackend/plugins/Databases.kt
@@ -1,35 +1,10 @@
 package com.barros9.tastytalesbackend.plugins
 
-import com.barros9.tastytalesbackend.fake.addRecipeCarbonara
-import com.barros9.tastytalesbackend.fake.addRecipeChickenCurry
-import com.barros9.tastytalesbackend.fake.addRecipeMargheritaPizza
-import com.barros9.tastytalesbackend.schema.*
 import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
 
 fun connectDatabase() {
     Database.connect(
-        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
-        user = "root",
-        driver = "org.h2.Driver",
-        password = "",
+        url = "jdbc:sqlite:src/main/resources/TastyTalesDatabase.db",
+        driver = "org.sqlite.JDBC"
     )
-
-    transaction {
-        SchemaUtils.create(
-            Recipes,
-            RecipesTranslations,
-            Ingredients,
-            IngredientsTranslations,
-            Instructions,
-            InstructionsTranslations,
-            Reviews,
-            ReviewsTranslations
-        )
-    }
-
-    addRecipeCarbonara()
-    addRecipeMargheritaPizza()
-    addRecipeChickenCurry()
 }
