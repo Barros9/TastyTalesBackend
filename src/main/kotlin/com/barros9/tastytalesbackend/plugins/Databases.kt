9259b873c8eb0d5bcd897bb2996a750a1407d390
barros9 <giu.barreca@gmail.com>
2024-08-20 22:07:21 +0200

    Added database in docker

--- src/main/kotlin/com/barros9/tastytalesbackend/plugins/Databases.kt
+++ src/main/kotlin/com/barros9/tastytalesbackend/plugins/Databases.kt
@@ -3,8 +3,10 @@ package com.barros9.tastytalesbackend.plugins
 import org.jetbrains.exposed.sql.Database
 
 fun connectDatabase() {
+    val dbPath = System.getenv("DATABASE_PATH") ?: "src/main/resources/TastyTalesDatabase.db"
+
     Database.connect(
-        url = "jdbc:sqlite:src/main/resources/TastyTalesDatabase.db",
+        url = "jdbc:sqlite:$dbPath",
         driver = "org.sqlite.JDBC"
     )
 }
